d3af5937e9108b49f38094b7e88c689824a50edc
Newburu <newburu@gmail.com>
2026-01-12 23:23:46 +0900

    Portfolio Previewの生成

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+    if user_signed_in?
+      @featured_user = current_user
+    else
+      @featured_user = User.joins(:projects).includes(:projects).order("RAND()").first
+    end
   end
 end
